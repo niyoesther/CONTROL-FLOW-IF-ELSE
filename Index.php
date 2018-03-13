1e287274504c3b1fb20beaac0e5e853972814fa2
niyoesther <35949152+niyoesther@users.noreply.github.com>
2018-03-13 19:14:37 +0800

    esther, add Rules and Regulations

---
+++ Index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+       <?php
+        $items = 1;
+        
+        if($items > 5) {
+          echo "The condition is true";
+        }
+        else{
+        echo "The condition is false";
+        }
+      ?>
+    </p>
+  </body>
+</html>
